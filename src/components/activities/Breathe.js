fbc204e9380cfe19a512468a7c41c15800a3d19d
github username <github email address>
2020-07-27 11:02:41 -0400

    incorporate breathe, play, remember

--- src/components/activities/Breathe.js
+++ src/components/activities/Breathe.js
@@ -1,19 +1,53 @@
 
 import React from 'react'
-
-const Breathe = () => {
+import Container from "react-bootstrap/Container"
+import Button from 'react-bootstrap/Button'
   
-  return (
-    <div className="breathe-container">
-     <h1>Don't Forget Your Breffs!!</h1>
-        Breathe Timer 
-        mp3 API
-      
+
+
+class Breathe extends React.Component {
+
+  
+  state = {
+    timer: 0
+  }
+
+  componentDidMount() {
+    this.setState({
+      timer: 0
+    })
+  }
+
+  clockTick = () => {
+    this.setState(prevState => ({
+      timer: prevState.timer + 1
+    }))
+  }
+
+  handleStartClick = () => {
+    this.interval = setInterval(this.clockTick, 1000)
+  }
+  
+  stopClock = () => {
+    clearInterval(this.interval)
+  }
+  
+  render() {
+   return(
+    <div className="breathe-container" >
+      <h1>Breathe</h1>
+      <h2>Close Your Eyes for 30 Seconds and Breathe Deeply</h2>
+      <Container>
+        <h2>{this.state.timer}</h2>
+        <Button onClick={this.handleStartClick}>Start Timer</Button>
+        <Button onClick={this.stopClock}>Stop Timer</Button>
+        <h2>Press Start Timer When Ready</h2>
+      </Container>
     </div>
-  )
+   )
+  }
 }
 
-export default Breathe
-
+export default Breathe;
 
 
---
+++ src/components/activities/Remember.js
@@ -0,0 +1,68 @@
+import React from 'react'
+import Form from 'react-bootstrap/Form'
+import Button from 'react-bootstrap/Button'
+import Container from 'react-bootstrap/Container'
+
+const initialState = {
+  date: "",
+  content: ""
+}
+
+class Remember extends React.Component {
+ 
+  state = initialState
+  
+  handleSubmit = e => {
+    e.preventDefault()
+    const {date, content} = this.state
+    fetch(`http://localhost:3002/api/v1/user_activity_logs`, {
+            method: 'POST',
+            headers: {
+                "Content-Type": "application/json",
+                Accept: "application/json"
+            },
+            body: JSON.stringify({
+               date,
+               content
+            })
+        })
+        .then(resp => resp.json())
+        .then(newPost => {
+            console.log(newPost)
+            this.setState({
+              initialState
+            })
+    })
+
+} 
+
+  handleChange = e => {
+    this.setState({
+      [e.target.name]: e.target.value
+    })
+ }
+  
+  render(){
+    console.log(this.state)
+    return(
+      <div>
+        <h1>Journal</h1>
+        <Container className="journal-container">
+          <Form onSubmit={event => this.handleSubmit(event)}>
+            <Form.Group controlId="formJournal">
+              <Form.Label>Enter The Date</Form.Label>
+                <Form.Control onChange={this.handleChange} value={this.state.date} name="date" type="date" placeholder="Release Your Thoughts"/>
+            </Form.Group>
+            <Form.Group controlId="formJournal">
+              <Form.Label>Type Your Thoughts</Form.Label>
+                <Form.Control onChange={this.handleChange} value={this.state.content} name="content" as="textarea" rows="6" type="text" placeholder="Release Your Thoughts"/>
+            </Form.Group>
+            <Button variant="primary" type="submit">Submit Entry</Button>
+          </Form>
+        </Container>
+    </div>
+  )
+  }   
+}
+
+export default Remember
